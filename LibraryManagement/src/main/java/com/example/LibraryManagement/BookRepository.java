38bb361911713bae28ad742e748467cfc452ab47
VU-PRITHIKA <prit22113.cs@rmkec.ac.in>
2025-07-19 18:59:46 +0530

    wrongly added

--- LibraryManagement/src/main/java/com/example/LibraryManagement/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.LIbraryManagement;
-
-public class BookRepository {
-    public void saveBook(String title) 
-    {
-        System.out.println("Saving book: " + title);
-    }
-}
--- MockingAndStubbing/src/main/java/com/example/MockingAndStubbing/ExternalApi.java
+++
@@ -1,6 +0,0 @@
-package com.example.MockingAndStubbing;
-
-public interface ExternalApi 
-{
-	String getData();
-}

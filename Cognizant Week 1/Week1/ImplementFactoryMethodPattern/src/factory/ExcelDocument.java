f6872baeb98cb60793f79e7768eb86f88b0048f2
PrithikaVaishnavi <115537774+PrithikaVaishnavi@users.noreply.github.com>
2025-07-21 12:46:11 +0530

    Delete Cognizant Week 1 directory

--- Cognizant Week 1/Week1/ImplementFactoryMethodPattern/src/factory/ExcelDocument.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-public class ExcelDocument implements Document{
-
-	public void open() {
-		System.out.println("Opening Excel Document");
-	}
-
-}
--- Cognizant Week 1/Week1/ImplementFactoryMethodPattern/src/factory/PdfDocument.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-public class PdfDocument implements Document{
-	@Override
-	public void open() {
-		// TODO Auto-generated constructor stub
-		System.out.println("Opening PDF Document");
-	}
-}

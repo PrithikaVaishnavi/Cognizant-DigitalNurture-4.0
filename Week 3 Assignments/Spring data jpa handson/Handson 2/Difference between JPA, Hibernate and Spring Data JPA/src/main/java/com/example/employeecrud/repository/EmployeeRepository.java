441f5a4304dc5ff66f1a86b10135b52be08a92b7
VU-PRITHIKA <prit22113.cs@rmkec.ac.in>
2025-07-28 15:03:27 +0530

    Add Week 3 Assignments

---
+++ Week 3 Assignments/Spring data jpa handson/Handson 2/Difference between JPA, Hibernate and Spring Data JPA/src/main/java/com/example/employeecrud/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.employeecrud.repository;
+
+import com.example.employeecrud.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> 
+{
+
+}
+
---
+++ Week 3 Assignments/Sprint Core Maven/Exercise4/IoCContainer/src/main/java/com/example/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+public class BookRepository 
+{
+    public BookRepository() 
+    {
+        System.out.println("BookRepository initialized");
+    }
+    public void displayBooks() 
+    {
+        System.out.println("BookRepository: Showing list of books...");
+    }
+}
